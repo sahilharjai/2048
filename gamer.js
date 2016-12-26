a2a6d5ecc6763173fdeb2a387936bd8d26d47a0c
Sahil Harjai <sahil.harjai99@gmail.com>
2016-12-26 22:41:20 +0530

    First Commit

--- gamer.js
+++ gamer.js
@@ -1,22 +1,87 @@
 var gamer = (function() {
     var mat = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+    var undo = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
     var list=[];
+    var d=0;
     var score = 0;
+    var undoScore=0;
+    var name={};
+    var best_score=0;
     var show2048dialog = 0;
-    function moveLeft(mat) {
-    	rotateBoard(mat);
-    	rotateBoard(mat);
-    	rotateBoard(mat);
-    	for(var i=0;i<4;i++)
-        { var start=0;
-           moveDown(mat,i,start);
-        }
-    	rotateBoard(mat);
-    	updateScore();
-    	redraw();
-    	styling();
+    function moveLeft() 
+    {
+    	var i=0;
+        while(i<=3)
+        {
+
+            var start=0,end=3,start2=0;
+            while(start<end)
+            {
+                if(mat[i][start]===0)
+                {
+                    var k=start;
+                    while(k<end)
+                    {
+                        mat[i][k]=mat[i][k+1];
+                        k++;
+                    }
+                    mat[i][end]=0;
+                    k=start;
+                    while(k<=end)
+                    {
+                        if(mat[i][k]!==0)
+                        {
+                            break;
+                        }
+                        k++;
+                    }
+                    if(k===end+1)
+                    {
+                        start=end;
+                    }
+                    else
+                    {
+                        start=start2;
+                    }
+                }
+
+                else if(mat[i][start]===mat[i][start+1])
+                {
+                    if(mat[i][start]!=0)
+                    {
+                        mat[i][start]=2*mat[i][start];
+                        mat[i][start+1]=0;
+                        score=score+mat[i][start];
+                        if(score>best_score)
+                        {
+                            best_score=score;
+                            $('#bscore').text('BEST SCORE:'+best_score);
+
+                        }
+                        start++;
+                        start2=start;
+                    }
+                }
+                else
+                {
+                    start++;
+                }
+            }
+            i++;
+        }           
+    	
     }
-    function styling(){
+    function moveleftrect()
+    {
+        moveLeft();
+        updateScore(); 
+        fillOneRandomEmptyCell();
+        redraw();
+        styling();
+
+    }
+    function styling()
+    {
     	var list1=[2,4,8,16,32,64,128,256,512,1024,2048];
     	for(var i=0;i<4;i++)
     	{
@@ -24,57 +89,63 @@ var gamer = (function() {
     		{ 
     			var temp=$('#tile_'+i+j).text();
 
+
     			if(temp==='0')
     			{
     				document.getElementById('tile_'+i+j).innerHTML=' ';
     			}
     			if(show2048dialog===0)
-    			{if(temp==='2048')
-    			{
-    				show2048dialog=true;
-    				show2048Dialog();
+    			{    if(temp==='2048')
+    			     {
+    				    show2048dialog=true;
+    				    show2048Dialog();
 
-    			}}
-    			for(var k=0;k<10;k++)
+    			     }
+                }
+    			for(var k=0;k<=10;k++)
     			{
     				$('#tile_'+i+j).removeClass('tile_'+list1[k]);
     			}
 
+
     			$('#tile_'+i+j).addClass('tile_'+temp);
+                name['tile_'+i+j] = temp;
     		}
     	}
+        name['score']=score;
+        name['bscore']=best_score;
+        localStorage.setItem('name', JSON.stringify(name));
     }
-    function moveRight(mat) {
-    	rotateBoard(mat);
-    	for(var i=0;i<4;i++)
-        { var start=0;
-           moveDown(mat,i,start);
-        }
-    	rotateBoard(mat);
-    	rotateBoard(mat);
-    	rotateBoard(mat);
+    function moveRight() 
+    {
+    	rotateBoard();
+    	rotateBoard();
+    	moveLeft(mat);
+    	rotateBoard();
+    	rotateBoard();
     	updateScore();
+        fillOneRandomEmptyCell();
     	redraw();
     	styling();
     }
-    function moveTop(mat) {
-    	rotateBoard(mat);
-    	rotateBoard(mat);
-    	for(var i=0;i<4;i++)
-        { var start=0;
-           moveDown(mat,i,start);
-        }
-    	rotateBoard(mat);
-    	rotateBoard(mat);
+    function moveTop() 
+    {
+        
+    	rotateBoard();
+    	rotateBoard();
+    	rotateBoard();
+    	moveLeft();
+    	rotateBoard();
     	updateScore();
+        fillOneRandomEmptyCell();
     	redraw();	
     	styling();   
 
     }
 
-    function rotateBoard(mat){
-    	
-    	var trans=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+    function rotateBoard()
+    {
+    var trans=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
     for(var i=0;i<4;i++)
     {
         for(var j=0;j<4;j++)
@@ -91,121 +162,53 @@ var gamer = (function() {
             mat[i][j]=trans[row][col];
             col--;
         }
-    }
+    }    	
 }
-    function updateScore(){
+    function updateScore()
+    {
     	document.getElementById('score').innerHTML="Score: "+score;
     }
 
-    function moveRight1(mat,col,start) {
-      if(start===4)
-      {
-        return;
-      }
-      moveRight1(mat,col,start+1)
-      for(var j=start-1;j>=0;j--)
-      {
-        if(mat[col][start]===0&&mat[col][j]!==0)
-        {
-          mat[col][start]=mat[col][j];
-          mat[j][col]=0;
-          continue;
-        }
-        else if(mat[col][start]!==0&&mat[col][j]===mat[col][start])
-        {
-          mat[col][start]=2*mat[col][start];
-              score=score+mat[col][start];
-              updateScore();
-              mat[col][j]=0;
-              break;
-        }
-        else if(mat[col][start]!==0&&mat[col][j]===0)
-          {
-              continue;
-          }
-          else {
-            break;
-          }
-
-      }
-      for(var j=0;j<3;j++)
-   {
-       if(mat[col][j]!==0&&mat[col][j+1]===0)
-       {
-        mat[col][j+1]=mat[col][j]
-        mat[col][j]=0;
-       }
-   }
-
-    }
-    function moveDown(mat,col,start) {
-    	if(start===4)
-      { 
-          return;
-      }
-      
-      moveDown(mat,col,start+1);
-
-      for(var j=start-1;j>=0;j--)
-      { 
-          if(mat[start][col]===0&&mat[j][col]!==0)
-          {
-              mat[start][col]=mat[j][col];
-              mat[j][col]=0;
-              continue;
-          }
-          else if(mat[start][col]!==0&&mat[j][col]===mat[start][col])
-          {
-              mat[start][col]=2*mat[start][col];
-              score=score+mat[start][col];
-              updateScore();
-              mat[j][col]=0;
-              break;
-          }
-          else if(mat[start][col]!==0&&mat[j][col]===0)
-          {
-              continue;
-          }
-          else {
-            break;
-          }
-
-      }
-   for(var j=0;j<3;j++)
-   {
-       if(mat[j][col]!==0&&mat[j+1][col]===0)
-       {
-           mat[j+1][col]=mat[j][col];
-           mat[j][col]=0;
-       }
-   }
+   
+    function moveDown() 
+    {
+    	rotateBoard();
+    	moveLeft(mat);
+    	rotateBoard();
+    	rotateBoard();
+    	rotateBoard();
+    	updateScore();
+        fillOneRandomEmptyCell();
+    	redraw();	
+    	styling();
 
     }
     // reflect state of mat
-    function redraw() {
-    	
+    function redraw()
+    {
         for(var i=0;i<4;i++)
         {
         	for(var j=0;j<4;j++)
         	{
         		document.getElementById('tile_'+i+j).innerHTML=mat[i][j]; 
-        		localStorage.setItem('tile_'+i+j,mat[i][j]);
         	}
         }
-            }
+    }
 
     // random number between 2 and 4  
     function getRandomValue() 
     {
          if(Math.random()>0.7)
          	{
-             return 4;
+                return 4;
          	}
-         	else{
+         	else
+            {
          		return 2;
          	}
     }
-    function getRandomEmptyCell(mat){
+    function getRandomEmptyCell()
+    {
     	list=[];
     	var c=0;
     	for(var i=0;i<4;i++)
@@ -225,14 +228,15 @@ var gamer = (function() {
     	return list[r];
     }
     
-    function fillOneRandomEmptyCell(mat) { //completed
+    function fillOneRandomEmptyCell() { //completed
         var coord=getRandomEmptyCell(mat);
         var value = getRandomValue();
         mat[coord[0]][coord[1]] = value;
     }
 
     // checks if gameover       completed
-    function isGameOver() {
+    function isGameOver() 
+    {
         for(var i=0;i<4;i++)
         {
             for(j=0;j<4;j++)
@@ -246,112 +250,151 @@ var gamer = (function() {
         return true;
     }
     
-    
-    function moveDownRect(mat){
-        for(var i=0;i<4;i++)
-        { var start=0;
-           moveDown(mat,i,start);
-        }
-        styling();
-        
-        return;
-
-    }
-    function moveRightRect(mat){
-        for(var i=0;i<4;i++)
-        { var start=0;
-           moveRight1(mat,i,start);
-        }
-        styling();
-        
-        return;
-
-    }
-
     // show Dialog for GameOver()
-    function showGameOverDialog() { // completed
-        $('#gameover').show();
-        document.getElementById("resetag").addEventListener('click',reset);
+    function showGameOverDialog()
+    { // completed
+        $('.gameover').show(); 
     }
 
     // show dialog for 2048
-    function show2048Dialog() {         //completed
+    function show2048Dialog() 
+    {  
+           //completed
      $('#won').show();
-     document.getElementById("resetag1").addEventListener('click',reset);
-     document.getElementById("continue").addEventListener('click',continue1);
+     show2048dialog = 0;
+     document.getElementById("resetag1").addEventListener('click',setgame);
     }
-    function continue1(){
-    	$('#won').hide();
-    	window.addEventListener('keydown', move.bind(null,mat));
-
+    function setgame()
+    {
+        reset();
+        return;
     }
 
-    function move(mat,e) {                     //completed
+    
+
+    function move(e) 
+    {                     //completed
         //depending upon keypress you call the respective function
-        fillOneRandomEmptyCell(mat);
-        redraw();
-
-        
-         e.preventDefault();
-        if (e.keyCode === 37) {
-            moveLeft(mat);
-        } else if (e.keyCode === 38) {
-            moveTop(mat);
-        } else if (e.keyCode === 39) {
-            moveRight(mat);
-        } else if (e.keyCode === 40) {
-            moveDownRect(mat);
+        if (e !== undefined) 
+        {
+        e.preventDefault();
         }
-        if (isGameOver()) {
 
+        for(var i=0;i<4;i++)
+        {
+            for(var j=0;j<4;j++)
+            {
+                undo[i][j]=mat[i][j];
+
+            }
+        }
+        d=0;
+        if (isGameOver()) 
+        {            
             showGameOverDialog();
         }
-        if (show2048dialog === true) {
+        if (show2048dialog === true) 
+        {
             show2048Dialog();
-            show2048dialog = false;
-        }
-        
-              
-                
             
-
+        }
+        else
+        {
+           if (e.keyCode === 37) 
+            {
+                moveleftrect();
+            } 
+            else if (e.keyCode === 38)
+            {
+                moveTop();
+            } 
+            else if (e.keyCode === 39) 
+            {
+                moveRight(mat);
+            } 
+            else if (e.keyCode === 40) 
+            {
+                moveDown();
+            } 
+        }
     }
+
+    function callUndo()
+    {
+
+        if(d===1)
+        {
+            return;
+        }
+        else
+        {
+            for(var i=0;i<4;i++)
+            {
+                for(var j=0;j<4;j++)
+                {
+                    mat[i][j]=undo[i][j];
+                }
+            }
+            score=undoScore;
+            updateScore();
+            redraw();   
+            styling();
+            d=1;
+        }
+    }
+
+
     function reset(e) {             //completed
-        if (e !== undefined) {
+        if (e !== undefined) 
+        {
             e.preventDefault();
         }
         mat = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
-        score = 0;
+        score=0;
+        d=1;
+        document.getElementById('score').innerHTML="Score: "+score
         fillOneRandomEmptyCell(mat);
         fillOneRandomEmptyCell(mat);
+        updateScore();
         redraw();
         styling();
-
-         $('#gameover').hide();
-          $('#won').hide();
-          window.addEventListener('keydown', move.bind(null,mat));
-    }
-    function init() {
-       // if(localStorage!==null)
-        {
-        	for(var i=0;i<4;i++)
-        	{
-        		for(var j=0;j<4;j++)
-        		{
-        			var temp=localStorage.getItem('tile_'+i+j);
-        			document.getElementById('tile_'+i+j).innerHTML=temp;
-        		}
-        	}
-        }    
-        reset();
-        document.getElementById("reset").addEventListener('click',reset);
-
-
-        $('#gameover').hide();
+        $('.gameover').hide();
         $('#won').hide();
-
+        return;
+          
+    }
+    function init(c) {   
+    var retrievedObject = JSON.parse(localStorage.getItem('name'));
+    if(retrievedObject!==null&&c===1)
+    {
+            for(var i=0;i<4;i++)
+            {
+                for(var j=0;j<4;j++)
+                {
+                mat[i][j]=Number(retrievedObject['tile_'+i+j]);  
+                }
+            }
+        score=Number(retrievedObject['score']);
+        best_score=Number(retrievedObject['bscore']);
+        $('#bscore').text('BEST SCORE:'+best_score);
+        updateScore(); 
+        redraw(mat);
+        styling();
+        c=0;
+    }
+    else
+    {
+        reset();
+    }
+     d=1;
+       document.getElementById("reset").addEventListener('click',setgame);
+       document.getElementById("undo").addEventListener('click',callUndo);
+       document.getElementById("resetag").addEventListener('click',setgame);
+        $('.gameover').hide();
+        $('#won').hide();
         // add reset method on click actions of all the reset elements
-        window.addEventListener('keydown', move.bind(null,mat));
+        window.addEventListener('keydown',move);
+        
     }
     return {
         init:init
